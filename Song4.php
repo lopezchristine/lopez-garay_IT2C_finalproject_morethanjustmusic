8c96ceed3167f0efc0045e032b75a18403cfd771
LopezChristineAnne <lopezchristine019@gmail.com>
2022-05-15 23:32:44 +0800

    fifth

---
+++ Song4.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>More Than Just Music</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/index.php"> More Than Just Music </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song5.php">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song6.php">Song 6</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song7.php">Song 7</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song8.php">Song 8</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song9.php">Song 9</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song10.php">Song 10</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song11.php">Song 11</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song12.php">Song 12</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song13.php">Song 13</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song14.php">Song 14</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song15.php">Song 15</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">More Than Just Music</h1>
+                <p class="lead">Worship is an it is well with my soul experience.</p>
+                <p class="lead">God created music to attach people to Him</p>
+
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>This I Believe (The Creed)</h2>";
+                        echo "<p class='lead'>Hillsong Worship Lyrics</p>";
+
+                        echo "<p> Our Father everlasting</p>";
+                        echo "<p> The all creating One</p>";
+                        echo "<p> God Almighty</p>";
+                        echo "<br/>";
+
+                        echo "<p> Through Your Holy Spirit</p>";
+                        echo "<p> Conceiving Christ the Son</p>";
+                        echo "<p> Jesus our Savior</p>";
+                        echo "<br?>";
+
+                        echo "<p> I believe in God our Father</p>";
+                        echo "<p> I believe in Christ the Son</p>";
+                        echo "<p> I believe in the Holy Spirit</p>";
+                        echo "<p> Our God is three in one</p>";
+                        echo "<p> I believe in the resurrection</p>";
+                        echo "<p> That we will rise again</p>";
+                        echo "<p> For I believe in the name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> Our Judge and our Defender</p>";
+                        echo "<p> Suffered and crucified</p>";
+                        echo "<p> Forever seated high</p>";
+                        echo "<br/>";
+
+                        echo "<p> Descended into darkness</p>";
+                        echo "<p> You rose in glorious life</p>";
+                        echo "<p> We're on our knees</p>";
+                        echo "<br?>";
+
+                        echo "<p> I believe in God our Father</p>";
+                        echo "<p> I believe in Christ the Son</p>";
+                        echo "<p> I believe in the Holy Spirit</p>";
+                        echo "<p> Our God is three in one</p>";
+                        echo "<p> I believe in the resurrection</p>";
+                        echo "<p> That we will rise again</p>";
+                        echo "<p> For I believe in the name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> I believe in You</p>";
+                        echo "<p> I believe You rose again</p>";
+                        echo "<p> I believe that Jesus Christ is Lord</p>";
+                        echo "<p> I believe</p>";
+                        echo "<p> I believe in You</p>";
+                        echo "<p> I believe You rose again</p>";
+                        echo "<p> I believe that Jesus Christ is Lord</p>";
+                        echo "<br?>";
+
+                        echo "<p> I believe in God our Father</p>";
+                        echo "<p> I believe in Christ the Son</p>";
+                        echo "<p> I believe in the Holy Spirit</p>";
+                        echo "<p> Our God is three in one</p>";
+                        echo "<p> I believe in the resurrection</p>";
+                        echo "<p> That we will rise again</p>";
+                        echo "<p> For I believe in the name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> I believe in God our Father</p>";
+                        echo "<p> I believe in Christ the Son</p>";
+                        echo "<p> I believe in the Holy Spirit</p>";
+                        echo "<p> Our God is three in one</p>";
+                        echo "<p> I believe in the resurrection</p>";
+                        echo "<p> That we will rise again</p>";
+                        echo "<p> For I believe in the name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> For I believe in the name of Jesus</p>";
+                        echo "<p> For I believe in the name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> I believe in life eternal</p>";
+                        echo "<p> I believe in the virgin birth</p>";
+                        echo "<p> I believe in the saints' communion</p>";
+                        echo "<p> And in Your holy Church</p>";
+                        echo "<p> I believe in the resurrection</p>";
+                        echo "<p> When Jesus comes again</p>";
+                        echo "<p> For I believe, in the name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> I believe in God our Father</p>";
+                        echo "<p> I believe in Christ the Son</p>";
+                        echo "<p> I believe in the Holy Spirit</p>";
+                        echo "<p> Our God is three in one</p>";
+                        echo "<p> I believe in the resurrection</p>";
+                        echo "<p> That we will rise again</p>";
+                        echo "<p> For I believe in the name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> I believe in God our Father</p>";
+                        echo "<p> I believe in Christ the Son</p>";
+                        echo "<p> I believe in the Holy Spirit</p>";
+                        echo "<p> Our God is three in one</p>";
+                        echo "<p> I believe in the resurrection</p>";
+                        echo "<p> That we will rise again</p>";
+                        echo "<p> For I believe in the name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> For I believe in the name of Jesus</p>";
+                        echo "<br?>";
+
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. More Than Just Music</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
+        
