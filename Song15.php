6b5be7035ceadee447536e5aa580e2ff02e6a958
LopezChristineAnne <lopezchristine019@gmail.com>
2022-05-16 01:18:03 +0800

    16th

---
+++ Song15.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>More Than Just Music</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/index.php"> More Than Just Music </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song5.php">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song6.php">Song 6</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song7.php">Song 7</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song8.php">Song 8</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song9.php">Song 9</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song10.php">Song 10</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song11.php">Song 11</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song12.php">Song 12</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song13.php">Song 13</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song14.php">Song 14</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song15.php">Song 15</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">More Than Just Music</h1>
+                <p class="lead">Worship is an it is well with my soul experience.</p>
+                <p class="lead">God created music to attach people to Him</p>
+
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>Oceans (Where Feet May Fail)</h2>";
+                        echo "<p class='lead'>Hillsong United Lyrics</p>";
+
+                        echo "<p> You call me out upon the waters</p>";
+                        echo "<p> The great unknown where feet may fail</p>";
+                        echo "<p> And there I find You in the mystery</p>";
+                        echo "<p> In oceans deep</p>";
+                        echo "<p> My faith will stand</p>";
+                        echo "<br/>";
+
+                        echo "<p> And I will call upon Your name</p>";
+                        echo "<p> And keep my eyes above the waves</p>";
+                        echo "<p> When oceans rise</p>";
+                        echo "<p> My soul will rest in Your embrace</p>";
+                        echo "<p> For I am Yours and You are mine</p>";
+                        echo "<br?>";
+
+                        echo "<p> Your grace abounds in deepest waters</p>";
+                        echo "<p> Your sovereign hand</p>";
+                        echo "<p> Will be my guide</p>";
+                        echo "<p> Where feet may Fail</p>";
+                        echo "<p> And fear surrounds me</p>";
+                        echo "<p> You've never failed</p>";
+                        echo "<p> And You won't start now</p>";
+                        echo "<br/>";
+
+                        echo "<p> So I will call upon Your name</p>";
+                        echo "<p> And keep my eyes above the waves</p>";
+                        echo "<p> When oceans rise</p>";
+                        echo "<p> My soul will rest in Your embrace</p>";
+                        echo "<p> For I am Yours and You are mine</p>";
+                        echo "<p> And You are mine</p>";
+                        echo "<br/>";
+
+                        echo "<p> Spirit lead me where my trust is without borders</p>";
+                        echo "<p> Let me walk upon the waters</p>";
+                        echo "<p> Wherever You would call me</p>";
+                        echo "<p> Take me deeper than my feet could ever wander</p>";
+                        echo "<p> And my faith will be made stronger</p>";
+                        echo "<p> In the presence of my Saviour</p>";               
+                        echo "<br/>";
+
+                        echo "<p> Spirit lead me where my trust is without borders</p>";
+                        echo "<p> Let me walk upon the waters</p>";
+                        echo "<p> Wherever You would call me</p>";
+                        echo "<p> Take me deeper than my feet could ever wander</p>";
+                        echo "<p> And my faith will be made stronger</p>";
+                        echo "<p> In the presence of my Saviour</p>";               
+                        echo "<br/>";
+
+                        echo "<p> Spirit lead me where my trust is without borders</p>";
+                        echo "<p> Let me walk upon the waters</p>";
+                        echo "<p> Wherever You would call me</p>";
+                        echo "<p> Take me deeper than my feet could ever wander</p>";
+                        echo "<p> And my faith will be made stronger</p>";
+                        echo "<p> In the presence of my Saviour</p>";               
+                        echo "<br/>";
+
+                        echo "<p> Spirit lead me where my trust is without borders</p>";
+                        echo "<p> Let me walk upon the waters</p>";
+                        echo "<p> Wherever You would call me</p>";
+                        echo "<p> Take me deeper than my feet could ever wander</p>";
+                        echo "<p> And my faith will be made stronger</p>";
+                        echo "<p> In the presence of my Saviour</p>";               
+                        echo "<br/>";
+
+                        echo "<p> Spirit lead me where my trust is without borders</p>";
+                        echo "<p> Let me walk upon the waters</p>";
+                        echo "<p> Wherever You would call me</p>";
+                        echo "<p> Take me deeper than my feet could ever wander</p>";
+                        echo "<p> And my faith will be made stronger</p>";
+                        echo "<p> In the presence of my Saviour</p>";               
+                        echo "<br/>";
+
+                        echo "<p> Spirit lead me where my trust is without borders</p>";
+                        echo "<p> Let me walk upon the waters</p>";
+                        echo "<p> Wherever You would call me</p>";
+                        echo "<p> Take me deeper than my feet could ever wander</p>";
+                        echo "<p> And my faith will be made stronger</p>";
+                        echo "<p> In the presence of my Saviour</p>";               
+                        echo "<br/>";
+
+                        echo "<p> IOh, Jesus, You're my God!</p>";
+
+                        echo "<p> I will call upon Your name</p>";
+                        echo "<p> Keep my eyes above the waves</p>";
+                        echo "<p> My soul will rest in Your embrace</p>";
+                        echo "<p> I am Yours and You are mine</p>";             
+                        echo "<br/>";
+
+
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. More Than Just Music</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
+        
