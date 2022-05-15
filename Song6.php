9b2ca9870af5acd0da784873363bec1b69810c41
LopezChristineAnne <lopezchristine019@gmail.com>
2022-05-15 23:56:40 +0800

    7th

---
+++ Song6.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>More Than Just Music</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/index.php"> More Than Just Music </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song5.php">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song6.php">Song 6</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song7.php">Song 7</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song8.php">Song 8</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song9.php">Song 9</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song10.php">Song 10</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song11.php">Song 11</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song12.php">Song 12</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song13.php">Song 13</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song14.php">Song 14</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/lopez_garay_IT2D_finalproject_morethanjustmusic/Song15.php">Song 15</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">More Than Just Music</h1>
+                <p class="lead">Worship is an it is well with my soul experience.</p>
+                <p class="lead">God created music to attach people to Him</p>
+
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+
+                        echo "<h2 class='text-primary'>What A Beautiful Name</h2>";
+                        echo "<p class='lead'>Hillsong Worship Lyrics</p>";
+
+                        echo "<p> You were the Word at the beginning</p>";
+                        echo "<p> One with God the Lord Most High</p>";
+                        echo "<p> Your hidden glory in creation</p>";
+                        echo "<p> Now revealed in You our Christ</p>";
+                        echo "<br/>";
+
+                        echo "<p> What a beautiful Name it is</p>";
+                        echo "<p> What a beautiful Name it is</p>";
+                        echo "<p> The Name of Jesus Christ my King</p>";
+                        echo "<br?>";
+
+                        echo "<p> What a beautiful Name it is</p>";
+                        echo "<p> Nothing compares to this</p>";
+                        echo "<p> What a beautiful Name it is</p>";
+                        echo "<p> The Name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> You didn't want Heaven without us</p>";
+                        echo "<p> So Jesus, You brought heaven down</p>";
+                        echo "<p> My sin was great, Your love was greate</p>";
+                        echo "<p> What could separate us now</p>";
+                        echo "<br?>";
+
+                        echo "<p> What a beautiful Name it is</p>";
+                        echo "<p> What a beautiful Name it is</p>";
+                        echo "<p> The Name of Jesus Christ my King</p>";
+                        echo "<br?>";
+
+                        echo "<p> What a wonderful Name it is</p>";
+                        echo "<p> Nothing compares to this</p>";
+                        echo "<p> What a wonderful Name it is</p>";
+                        echo "<p> The Name of Jesus</p>";
+                        echo "<p> What a beautiful Name it is</p>";
+                        echo "<p> The Name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> Death could not hold You, the veil tore before You</p>";
+                        echo "<p> You silenced the boast of sin and grave</p>";
+                        echo "<p> The heavens are roaring the praise of Your glory</p>";
+                        echo "<p> For You are raised to life again</p>";
+                        echo "<br?>";
+
+                        echo "<p> You have no rival, You have no equal</p>";
+                        echo "<p> Now and forever, God, You reign</p>";
+                        echo "<p> Yours is the Kingdom, Yours is the glory</p>";
+                        echo "<p> Yours is the Name above all names</p>";
+                        echo "<br?>";
+
+                        echo "<p> What a powerful Name it is</p>";
+                        echo "<p> What a powerful Name it is</p>";
+                        echo "<p> The Name of Jesus Christ my King</p>";
+                        echo "<br?>";
+
+                        echo "<p> What a powerful Name it is</p>";
+                        echo "<p> Nothing can stand against</p>";
+                        echo "<p> What a powerful Name it is</p>";
+                        echo "<p> The Name of Jesus</p>";
+                        echo "<br?>";
+
+                        echo "<p> What a powerful Name it is</p>";
+                        echo "<p> Nothing can stand against</p>";
+                        echo "<p> What a powerful Name it is</p>";
+                        echo "<p> The Name of Jesus</p>";
+                        echo "<br?>";
+
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. More Than Just Music</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
+        
